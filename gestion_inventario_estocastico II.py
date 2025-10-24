1824bf845124104217e808ac4b8b3f6792c26de6
jedi1lopez <jedi1lopez@gmail.com>
2025-10-24 10:40:09 -0400

    Refactor author information in comments
    Correción-01

--- gestion_inventario_estocastico II.py
+++ gestion_inventario_estocastico II.py
@@ -1,6 +1,7 @@
-# Autores: Dennis Zavala & Jetro López - UNESR, Venezuela
+# Autores: Dennis Zavala & Jetro López - 
 # Fecha: 2025-04-05
 Cantidad óptima de pedido (Q*): 712.22 unidades
 Costo total promedio simulado: Bs. 11332.46
 
 Desviación estándar del costo: Bs. 521.92
+
